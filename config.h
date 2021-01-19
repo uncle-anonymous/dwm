e6a8e1a42e06490d238bf7cec31fd78874b65460
Your Name <you@example.com>
2021-01-19 20:24:43 +0800

    browser

--- config.h
+++ config.h
@@ -84,7 +84,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 //static const char *dmenucmd[] = { "rofi", "-show", "run", NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *browsercmd[]  = { "google-chrome-stable", NULL };
+static const char *browsercmd[]  = { "/home/david/scripts/browser.sh", NULL };
 
 static const char *upvol[]   = { "/home/david/scripts/vol-up.sh",  NULL };
 static const char *downvol[] = { "/home/david/scripts/vol-down.sh",  NULL };
